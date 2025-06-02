afe7f8a0603bd72459cdb40d2fbb7b8abbbb8748
Rosa <rosapassaro28@gmail.com>
2025-06-02 10:44:05 +0200

    aggiunte view e main che carica la view

--- src/main/java/it/unisa/diem/model/gestione/analisi/Stopword.java
+++ src/main/java/it/unisa/diem/model/gestione/analisi/Stopword.java
@@ -2,4 +2,8 @@ package it.unisa.diem.model.gestione.analisi;
 
 public class Stopword {
     private final String parola;        //oppure un insieme di parole?????????
+
+    public Stopword(String parola) {
+        this.parola = parola;
+    }
 }
--- src/main/java/it/unisa/diem/model/gestione/sessione/Domanda.java
+++ src/main/java/it/unisa/diem/model/gestione/sessione/Domanda.java
@@ -8,4 +8,11 @@ public class Domanda {
     private final String tipologia;     //tipo enum????????
     private String rispostaInserita;
     private final String testo;
+
+    public Domanda(Sessione sessione, int valore, String tipologia, String testo) {
+        this.sessione = sessione;
+        this.valore = valore;
+        this.tipologia = tipologia;
+        this.testo = testo;
+    }
 }
