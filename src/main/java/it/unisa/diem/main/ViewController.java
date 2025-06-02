ca58d7a978cc9ba150f78d35dff4631ed48e9b9d
Vincenzo <v.nunziato2@studenti.unisa.it>
2025-06-02 23:53:01 +0200

    adesso è possibile switchare le schermate login/registrazione

--- src/main/java/it/unisa/diem/main/ViewController.java
+++ src/main/java/it/unisa/diem/main/ViewController.java
@@ -1,5 +1,38 @@
 package it.unisa.diem.main;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
 public class ViewController {
+    @FXML
+    private VBox loginPane, registerPane;
+    @FXML private TextField loginUsernameField;
+    @FXML private PasswordField loginPasswordField;
+    @FXML private TextField registerEmailField, registerUsernameField;
+    @FXML private PasswordField registerPasswordField, registerConfirmField;
+
+    @FXML
+    private void handleLogin() {
+        System.out.println("Login premuto");
+    }
+
+    @FXML
+    private void handleRegister() {
+        System.out.println("Registrazione premuta");
+    }
+
+    @FXML
+    public void showRegister() {
+        loginPane.setVisible(false);
+        registerPane.setVisible(true);
+    }
+
+    @FXML
+    public void showLogin() {
+        registerPane.setVisible(false);
+        loginPane.setVisible(true);
+    }
 
 }
