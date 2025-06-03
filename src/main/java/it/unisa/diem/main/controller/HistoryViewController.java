10a2d0afc431cadf15a20edfb6d958676bc05b4a
Vincenzo <v.nunziato2@studenti.unisa.it>
2025-06-03 17:09:27 +0200

    aggiunto pulsanti di avanti e dietro tra i menu

--- src/main/java/it/unisa/diem/main/controller/HistoryViewController.java
+++ src/main/java/it/unisa/diem/main/controller/HistoryViewController.java
@@ -1,4 +1,19 @@
 package it.unisa.diem.main.controller;
 
+import it.unisa.diem.utility.SceneLoader;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class HistoryViewController {
+    @FXML private Button leaderboardButton;
+    @FXML private Button backButton;
+
+    public void goToMainMenu(ActionEvent actionEvent) {
+        SceneLoader.load("HomeMenuView.fxml", backButton);
+    }
+
+    public void goToLeaderboard(ActionEvent actionEvent) {
+        SceneLoader.load("LeaderboardView.fxml", leaderboardButton);
+    }
 }
--- src/main/java/it/unisa/diem/main/controller/HomeMenuViewController.java
+++ src/main/java/it/unisa/diem/main/controller/HomeMenuViewController.java
@@ -1,4 +1,25 @@
 package it.unisa.diem.main.controller;
 
+import it.unisa.diem.utility.SceneLoader;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+
 public class HomeMenuViewController {
+    @FXML private Button newGameButton;
+    @FXML private Button leaderboardButton;
+    @FXML private Button historyButton;
+
+    public void goToNewGame(ActionEvent actionEvent) {
+        SceneLoader.load("LoadView.fxml", newGameButton);
+    }
+
+    public void goToLeaderboard(ActionEvent actionEvent) {
+        SceneLoader.load("LeaderboardView.fxml", leaderboardButton);
+    }
+
+    public void goToHistory(ActionEvent actionEvent) {
+        SceneLoader.load("HistoryView.fxml", historyButton);
+    }
 }
