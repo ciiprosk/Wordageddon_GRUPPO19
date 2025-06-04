765451badb2ae6e66a2fa70c72cf171f8646b7e9
Antonio Vitale <a.vitale132@studenti.unisa.it>
2025-06-04 21:19:13 +0200

    Aggiunti fxid e corrispettivi nel controller

--- src/main/java/it/unisa/diem/main/controller/AdminScreenView.java
+++ src/main/java/it/unisa/diem/main/controller/AdminScreenView.java
@@ -1,4 +1,21 @@
 package it.unisa.diem.main.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextArea;
+
 public class AdminScreenView {
+
+    @FXML private Button importButton;
+    @FXML private Button backButton;
+    @FXML private TextArea stopwordsArea;
+    @FXML private CheckBox checkArticles;
+    @FXML private CheckBox checkPrepositions;
+    @FXML private CheckBox checkToBe;
+    @FXML private CheckBox checkToHave;
+    @FXML private CheckBox checkCon;
+    @FXML private CheckBox checkPronouns;
+
+
 }
