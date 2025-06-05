cbc75a37d9fd76a9c38eb5a8068b51b4b1030a6d
Antonio Vitale <a.vitale132@studenti.unisa.it>
2025-06-06 00:58:02 +0200

    Aggiunto immagini simpatiche

--- src/main/java/it/unisa/diem/main/controller/HistoryViewController.java
+++ src/main/java/it/unisa/diem/main/controller/HistoryViewController.java
@@ -1,13 +1,31 @@
 package it.unisa.diem.main.controller;
 
+import it.unisa.diem.main.Main;
 import it.unisa.diem.utility.SceneLoader;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class HistoryViewController {
     @FXML private Button leaderboardButton;
     @FXML private Button backButton;
+
+    @FXML
+    public void initialize() {
+        Image back = new Image(Main.class.getClassLoader().getResourceAsStream("immagini/yellowbackarrow.png"));
+        ImageView backView = new ImageView(back);
+        backView.setFitWidth(30);
+        backView.setFitHeight(30);
+        backButton.setGraphic(backView);
+
+        Image leader = new Image(Main.class.getClassLoader().getResourceAsStream("immagini/trophy.png"));
+        ImageView leaderView = new ImageView(leader);
+        leaderView.setFitWidth(30);
+        leaderView.setFitHeight(30);
+        leaderboardButton.setGraphic(leaderView);
+    }
 
     public void goToMainMenu(ActionEvent actionEvent) {
         SceneLoader.load("HomeMenuView.fxml", backButton);
--- src/main/java/it/unisa/diem/main/controller/LeaderboardViewController.java
+++ src/main/java/it/unisa/diem/main/controller/LeaderboardViewController.java
@@ -1,10 +1,13 @@
 package it.unisa.diem.main.controller;
 
+import it.unisa.diem.main.Main;
 import it.unisa.diem.utility.SceneLoader;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class LeaderboardViewController {
     @FXML private Button historyButton;
@@ -14,6 +17,18 @@ public class LeaderboardViewController {
 
     @FXML
     public void initialize() {
+        Image back = new Image(Main.class.getClassLoader().getResourceAsStream("immagini/yellowbackarrow.png"));
+        ImageView backView = new ImageView(back);
+        backView.setFitWidth(30);
+        backView.setFitHeight(30);
+        backButton.setGraphic(backView);
+
+        Image history= new Image(Main.class.getClassLoader().getResourceAsStream("immagini/history.png"));
+        ImageView historyView = new ImageView(history);
+        historyView.setFitWidth(30);
+        historyView.setFitHeight(30);
+        historyButton.setGraphic(historyView);
+
         difficoltaComboBox.getItems().addAll("EASY", "NORMAL", "HARD");
 
 
