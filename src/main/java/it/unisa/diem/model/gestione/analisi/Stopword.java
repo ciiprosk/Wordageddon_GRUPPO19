16e4f2ddf7cb15f786c6d9fb71d3bef726cab931
Rosa <rosapassaro28@gmail.com>
2025-06-06 14:57:07 +0200

    stopword

--- src/main/java/it/unisa/diem/model/gestione/analisi/Stopword.java
+++ src/main/java/it/unisa/diem/model/gestione/analisi/Stopword.java
@@ -6,7 +6,15 @@ import java.util.List;
 public class Stopword {
     private final List<String> parole;        //oppure un insieme di parole?????????
 //array d default --> preposizioni, articoli, pronomi, verbi essere e avere, connettivi e congiunzioni
+    private String[] articoli= {"il","la","lo","i","e","un","una","uno","un'","une","le","l'","l"};
+    private String[] preposizioni={"di","a", "da", "in", "con", "su", "per", "tra", "fra", "della", "delle", "degli"};
+    private String[] pronomi;
+    private String[] verbiEssereAvere;
+    private String[] connettivi;
+    private String[] congiunzioni;
     public Stopword() {
         parole=new ArrayList<>();
     }
+
+
 }
---
+++ src/main/java/it/unisa/diem/model/gestione/analisi/stopword/StopwordITA.java
@@ -0,0 +1,4 @@
+package it.unisa.diem.model.gestione.analisi.stopword;
+
+public class StopwordITA {
+}
