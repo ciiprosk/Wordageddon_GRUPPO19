9b27ff6178b9ede4369458785a4c673863e22891
Omar Silano <silanoomar@gmail.com>
2025-06-07 20:38:58 +0200

    mo si può giocare con UtenteDAO

--- src/main/java/it/unisa/diem/dao/postgres/DomandaDAOPostgres.java
+++ src/main/java/it/unisa/diem/dao/postgres/DomandaDAOPostgres.java
@@ -1,22 +1,68 @@
 package it.unisa.diem.dao.postgres;
 import it.unisa.diem.dao.interfacce.DAO;
 import it.unisa.diem.dao.interfacce.DomandaDAO;
+import it.unisa.diem.exceptions.DBException;
 import it.unisa.diem.model.gestione.sessione.Domanda;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-public class DomandaDAOPostgres implements DAO<Domanda> {
+public class DomandaDAOPostgres implements DomandaDAO {
+
+    private final String url;
+    private final String user;
+    private final String pass;
+
+    public DomandaDAOPostgres(String url, String user, String pass) {
+        this.url = url;
+        this.user = user;
+        this.pass = pass;
+    }
 
 
-    public Optional<Domanda> selectById(long id) {
-        return Optional.empty();
+    public Optional<Domanda> selectByTitle(String titolo) {
+
+        Optional<Domanda> result = Optional.empty();
+
+        String query = "SELECT * FROM Domanda WHERE titolo = ?";
+
+        try (Connection connection = DriverManager.getConnection(url, user, pass);
+
+            PreparedStatement cmd = connection.prepareStatement (query) ){
+
+            cmd.setString(1, titolo);
+
+            ResultSet rs = cmd.executeQuery();
+
+            Domanda domanda = null;
+
+            if (rs.next()) {
+                domanda = getDomanda(rs);
+            }
+
+            result = Optional.ofNullable(domanda);
+
+        } catch (SQLException e) {
+            throw new DBException("Errore select domanda",e);     //la creiamo?
+        }
+
+        return result;
+
     }
 
     @Override
     public List<Domanda> selectAll() {
-        return Collections.emptyList();
+
+        List<Domanda> domande = new ArrayList<>();
+
+        String query = "SELECT * FROM Domanda";
+
+
+
+        return domande;
     }
 
     //questo metodo inserisce all'interno del db: nome del documento, posizione relativa del file, estensione, limgua
@@ -32,6 +78,16 @@ public class DomandaDAOPostgres implements DAO<Domanda> {
 
     @Override
     public void delete(Domanda domanda) {
+
+    }
+
+    private Domanda getDomanda(ResultSet rs) throws SQLException {
+
+        Domanda domanda = null;
+
+        //da implementare
+
+        return domanda;
 
     }
 
