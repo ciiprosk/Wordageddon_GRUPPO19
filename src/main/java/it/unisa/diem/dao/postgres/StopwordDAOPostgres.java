087409c36da59d83c2b5c6f4b4276525ab8cd53d
Rosa <rosapassaro28@gmail.com>
2025-06-08 18:47:30 +0200

    domanda modifictata

--- src/main/java/it/unisa/diem/dao/postgres/StopwordDAOPostgres.java
+++ src/main/java/it/unisa/diem/dao/postgres/StopwordDAOPostgres.java
@@ -14,17 +14,16 @@ public class StopwordDAOPostgres implements StopwordDAO {
 
     /**
      * Costruttore della classe che recupera la stringa di connessione del db, user, pass.
-     * @param url Parametro fondamentale per la connessione al database, è una stringa contenente il database .
-     * @param user
-     * @param pass
+     * @param url Parametro fondamentale per la connessione al database, è una stringa contenente il database e jdbc.
+     * @param user Parametro per eseguire l'accesso al database, è l'utente.
+     * @param pass Parametro per eseguire l'accesso al database, è la password.
      */
     public StopwordDAOPostgres(String url, String user, String pass) {
         this.url=url;
         this.user=user;
         this.pass=pass;
     }
-    //stopword ha id di documento--> come lo posso ricavare?al nome del documento immagio
-    //faccio una query che mi restituisce l'id della query
+
     public Optional<StopwordManager> selectById(long id) {
         return Optional.empty();
     }
@@ -34,6 +33,8 @@ public class StopwordDAOPostgres implements StopwordDAO {
         return Collections.emptyList();
     }
 
+    //stopword ha id di documento--> come lo posso ricavare?al nome del documento immagio
+    //faccio una query che mi restituisce l'id della query
     @Override
     public void insert(StopwordManager stopword) {
 
