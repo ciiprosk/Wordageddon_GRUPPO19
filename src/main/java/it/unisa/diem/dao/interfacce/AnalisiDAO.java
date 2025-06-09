7954af5caeb69f8793844a3992bd712f9b2e3a84
Rosa <rosapassaro28@gmail.com>
2025-06-09 23:18:55 +0200

    terminato AnalisiDAOPostgres.java

--- src/main/java/it/unisa/diem/dao/interfacce/AnalisiDAO.java
+++ src/main/java/it/unisa/diem/dao/interfacce/AnalisiDAO.java
@@ -1,6 +1,13 @@
 package it.unisa.diem.dao.interfacce;
 
+import it.unisa.diem.exceptions.DBException;
 import it.unisa.diem.model.gestione.analisi.Analisi;
 
+import java.sql.SQLException;
+import java.util.Optional;
+
 public interface AnalisiDAO extends DAO<Analisi>{
+
+    public Optional<Analisi> selectAnalisiByTitle(String titolo) throws SQLException, DBException;
+    public void update(Analisi a, String oldTitolo) throws SQLException, DBException;
 }
--- src/main/java/it/unisa/diem/dao/postgres/AnalisiDAOPostgres.java
+++ src/main/java/it/unisa/diem/dao/postgres/AnalisiDAOPostgres.java
@@ -1,5 +1,6 @@
 package it.unisa.diem.dao.postgres;
 
+import it.unisa.diem.dao.interfacce.AnalisiDAO;
 import it.unisa.diem.dao.interfacce.DAO;
 import it.unisa.diem.exceptions.DBException;
 import it.unisa.diem.model.gestione.analisi.Analisi;
@@ -12,7 +13,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-public class AnalisiDAOPostgres implements DAO<Analisi> {
+public class AnalisiDAOPostgres implements AnalisiDAO {
     private String url;
     private String user;
     private String password;
@@ -24,8 +25,41 @@ public class AnalisiDAOPostgres implements DAO<Analisi> {
     }
 
 
-    public Optional<Analisi> selectById(long id) {
-        return Optional.empty();
+    @Override
+    public Optional<Analisi> selectAnalisiByTitle(String titolo) throws SQLException, DBException{
+        Optional<Analisi> analisi = Optional.empty();
+        //query: la query fa una select per nome analisi
+        String query= "SELECT * FROM analisi where nome = ?";
+        try(Connection conn=DriverManager.getConnection(url, user, password);
+             PreparedStatement ps= conn.prepareStatement(query);){
+             ps.setString(1, titolo);
+             ResultSet rs= ps.executeQuery();
+
+             Analisi a=null;
+             if(rs.next()){
+                 a= getAnalisi(rs);
+             }
+            analisi = Optional.ofNullable(a);
+
+        }catch(SQLException e){
+            throw new DBException("Errore nel databse");
+        }
+        return analisi;
+    }
+
+    @Override
+    public void update(Analisi a, String oldTitolo) throws SQLException, DBException { //c'è un trigger che cambia anche documento e vale il viceversa
+        String query = "UPDATE analisi SET nome = ? WHERE nome = ?";
+        try(Connection con=DriverManager.getConnection(url, user, password);
+        PreparedStatement ps=con.prepareStatement(query);){
+            ps.setString(1, a.getTitolo());
+            ps.setString(2, oldTitolo);
+            int lines=ps.executeUpdate();
+            if(lines == 0) throw new DBException("Errore: nessuna riga modificata");
+
+        }catch(SQLException e){
+            throw new DBException("Errore nel databse, Impossibile inserire username",e);
+        }
     }
 
     @Override
@@ -66,6 +100,8 @@ public class AnalisiDAOPostgres implements DAO<Analisi> {
 
     @Override
     public void update(Analisi analisi) {
+        //faccoi query che modifica il nome di analisi
+
 
     }
 
@@ -96,4 +132,6 @@ public class AnalisiDAOPostgres implements DAO<Analisi> {
         a = new Analisi(new Documento(documento,lingua, difficolta), null); //non mi interessano le stopword
         return a;
     }
+
+
 }
