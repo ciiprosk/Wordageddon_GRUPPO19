6c10a9ab34ffc0df01324762e1187af2662b55ad
Rosa <rosapassaro28@gmail.com>
2025-06-10 15:55:43 +0200

    modificato documento e analisi per mdoficiare il nome del file

--- src/main/java/it/unisa/diem/dao/postgres/AnalisiDAOPostgres.java
+++ src/main/java/it/unisa/diem/dao/postgres/AnalisiDAOPostgres.java
@@ -61,7 +61,7 @@ public class AnalisiDAOPostgres implements AnalisiDAO {
  */
     @Override
     public List<Analisi> selectAll() throws DBException{
-        List<Analisi> analisi= null;
+        List<Analisi> analisi=  new ArrayList<>();
         String query = "SELECT * FROM analisi";
         try(Connection con = DriverManager.getConnection(url, user, password);
             Statement st=con.createStatement();){
@@ -96,7 +96,7 @@ public class AnalisiDAOPostgres implements AnalisiDAO {
 
     @Override
     public void update(Analisi analisi) throws DBException{
-        String query = "UPDATE analisi SET nome = ? percorso = ? WHERE nome = ?";
+        String query = "UPDATE analisi SET nome = ?, percorso = ? WHERE nome = ?";
         try(Connection con=DriverManager.getConnection(url, user, password);
             PreparedStatement ps=con.prepareStatement(query);){
             ps.setString(1, analisi.getTitolo());
