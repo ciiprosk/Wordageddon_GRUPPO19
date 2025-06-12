47570ca2bd1917a6e2fe57a4fc8f7ebb2cfcc3cb
ciiprosk <r.passaro5@studenti.unisa.it>
2025-06-12 16:51:38 +0200

    caricate tutte le cartelleeeeeee

--- src/main/java/it/unisa/diem/test/DocumentoAnalisiTest.java
+++ src/main/java/it/unisa/diem/test/DocumentoAnalisiTest.java
@@ -11,8 +11,8 @@ import java.io.IOException;
 public class DocumentoAnalisiTest {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         System.out.println("ciao");
-        Documento documento = new Documento("diocane", Lingua.ITA, Difficolta.DIFFICILE);
-        documento.convertiTxtToBin(new File("data/ITA/intermedio/principessa.txt"));
+        Documento documento = new Documento("PROVA", Lingua.ENG, Difficolta.FACILE);
+        documento.convertiTxtToBin(new File("data/ENG/facile/joker.txt"));
         Analisi a=new Analisi(documento);
         a.analizza();
         System.out.println(a.getFrequenzeTesto());
